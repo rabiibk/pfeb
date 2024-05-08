897ea3504e046ffa929e95bfbdd0b2ae8c84135e
rabiibk <rabii.benkhlifa@esprit.tn>
2024-05-08 19:08:33 +0200

    back complet 22

---
+++ src/main/java/com/example/angular/springbootcrudapi/configuration/DataInitializer.java
@@ -0,0 +1,29 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import com.votrepackage.Employee;
+import com.votrepackage.EmployeeRepository;
+
+@Component
+public class DataInitializer implements ApplicationRunner {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        // Vérifiez si l'employé par défaut n'existe pas déjà
+        if (!employeeRepository.existsByName("admin")) {
+            // Créez l'employé par défaut
+            Employee admin = new Employee();
+            admin.setName("admin");
+            admin.setMatricule(12304);
+            admin.setPhone(58362749);
+            admin.setMail("rabii.benkhlifa@esprit.tn");
+            admin.setDepartment("RH");
+            // Enregistrez l'employé dans la base de données
+            employeeRepository.save(admin);
+        }
+    }
+}
