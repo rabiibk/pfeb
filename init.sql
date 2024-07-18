383bb6ca92dd5d75a7361650f8fe00cdea2f44c1
rabiibk <rabii.benkhlifa@esprit.tn>
2024-07-18 19:43:17 +0200

    push final

--- init.sql
+++ init.sql
@@ -1,3 +1,11 @@
+CREATE TABLE IF NOT EXISTS employee (
+                                        id INT PRIMARY KEY,
+                                        name VARCHAR(255),
+    email VARCHAR(255),
+    phone INT,
+    matricule INT,
+    department VARCHAR(255)
+    );
 
-INSERT INTO employee (id , name, email , phone, matricule , department)
-VALUES (1 , 'admin', 'rabii.benkhlifa@esprit.tn' , 58362749,  12304 , 'RH');
+INSERT INTO employee (id, name, email, phone, matricule, department)
+VALUES (1, 'admin', 'rabii.benkhlifa@esprit.tn', 58362749, 12304, 'RH');
